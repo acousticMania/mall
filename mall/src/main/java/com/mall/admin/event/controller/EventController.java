7305917a8b479b0577c710e05eab1ee97ca0410b
Seo.myeongseok <rose@naonsoft.com>
2017-03-30 22:00:24 +0900

    # 15-1 Git테스트 event반영

---
+++ mall/src/main/java/com/mall/admin/event/controller/EventController.java
@@ -0,0 +1,15 @@
+
+package com.mall.admin.event.controller;
+
+import org.springframework.stereotype.Controller;
+
+
+
+@Controller
+public class EventController {
+	
+	public String testGnrKey() {
+		return "";
+	}
+	
+}
