93316c278b8378de3f7766dc484ce163ff6f4011
Seo.myeongseok <rose@naonsoft.com>
2017-04-10 21:14:53 +0900

    #03 EventList ajax 작업중

--- mall/src/main/java/com/mall/admin/event/controller/EventController.java
+++ mall/src/main/java/com/mall/admin/event/controller/EventController.java
@@ -1,15 +1,26 @@
 
 package com.mall.admin.event.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class EventController {
 	
-	@RequestMapping("/admin/event/eventList")
-	public String viewEvent() {
+	private static final Logger logger = LoggerFactory.getLogger(EventController.class);
+	
+	@RequestMapping("/admin/event/viewEventList")
+	public String viewEventList() {
 		return "/admin/event/eventList.tiles";
 	}
 	
+	@RequestMapping("/admin/event/selectEventList")
+	public ModelAndView eventList() {
+		
+		ModelAndView mv = new ModelAndView("/admin/event/selectEventList");
+		return mv;
+	}
 }
